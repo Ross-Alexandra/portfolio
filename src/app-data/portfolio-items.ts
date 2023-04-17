8af1ccc07e0ec679b957ac06201896bcb80c6d6f
Ross Alexandra <Ross-Alexandra@outlook.com>
2023-04-16 20:59:56 -0700

    Added portfolio pages

--- src/app-data/portfolio-items.ts
+++ src/app-data/portfolio-items.ts
@@ -1,1 +1,89 @@
-export {};
+import { ConnectedCupBanner, NHL22Banner, PickBanBanner, ReplayAnalyzerSlide1, ReplayAnalyzerSlide2, ReplayAnalyzerSlide3, ReplayAnalyzerSlide4, RewardYourselfSlide1, RewardYourselfSlide2, RewardYourselfSlide3, RewardYourselfSlide4, RewardYourselfSlide5, RewardYourselfSlide6, RewardYourselfSlide7, StratBook, StratbookSlide1, StratbookSlide2, StratbookSlide3, StratbookSlide4, StratbookSlide5, StratbookSlide6, StratbookSlide7, StratbookSlide8, ZotacCupBanner, ePLBanner } from "../assets";
+
+interface PrimaryTile {
+    title: string;
+    image: string;
+    externalLink?: string;
+}
+
+interface Button {
+    text: string;
+    link: string;
+}
+
+export interface PortfolioPageInfo {
+    title: string;
+    intro: string;
+    backgroundImage: string;
+    primaryTiles: PrimaryTile[];
+    featureTitle?: string;
+    featureDescription?: string;
+    featureImages: string[];
+    buttons: Button[];
+}
+
+export const PortfolioPages: PortfolioPageInfo[] = [{
+    title: 'Battlefy',
+    intro: 'During my time at Battlefy, I had the opportunity to work on several exciting projects which helped enhance the overall experience for our users. Four of my proudest projects include NHL 2022, Zotac Cup, Connected Club Cub, and ePremier League.',
+    backgroundImage: 'https://source.unsplash.com/I45hdPF5Na0',
+    primaryTiles: [{
+        title: 'NHL 2022',
+        image: NHL22Banner,
+        externalLink: 'https://battlefy.com/nhl-2022'
+    }, {
+        title: 'Zotac Cup',
+        image: ZotacCupBanner,
+        externalLink: 'https://battlefy.com/zotac-cup'
+    }, {
+        title: 'Connected Club Cup',
+        image: ConnectedCupBanner,
+        externalLink: 'https://battlefy.com/connectedclubcup/home'
+    }, {
+        title: 'ePremier League',
+        image: ePLBanner,
+        externalLink: 'https://e.premierleague.com/'
+    }],
+    featureTitle: 'Pick Ban System',
+    featureDescription: "I also had the opportunity to work on numerous internal projects, the biggest of which was the platform's Pick Ban system. This system allows tournament organizers to setup a Pick Ban draft for their Valorant tournaments.",
+    featureImages: [PickBanBanner],
+    buttons: [{
+        link: '/experience#battlefy',
+        text: 'My Time at Battlefy'
+    }]
+}, {
+    title: 'StratBook',
+    intro: 'StratBook is a web application that allows users to create and share their own strategies for Rainbow Six Siege. Users can create their own strategies, then export them to an image for easy sharing.',
+    primaryTiles: [],
+    backgroundImage: StratBook,
+    featureImages: [StratbookSlide1, StratbookSlide2, StratbookSlide3, StratbookSlide4, StratbookSlide5, StratbookSlide6, StratbookSlide7, StratbookSlide8],
+    buttons: [{
+        link: 'https://stratbook.gg',
+        text: 'Visit StratBook.gg'
+    }, {
+        link: '/experience#stratbook',
+        text: 'Learn About StratBook'
+    }],
+}, {
+    title: 'Reward Yourself',
+    intro: 'Reward yourself is a mobile app that allows users to earn points for completing tasks, which they can redeem for rewards they have created for themselves. This app was created both as a way to learn about mobile development, and as a tool to help mentor a friend. This app is not currently available on the app store, but we are working on getting it there.',
+    primaryTiles: [],
+    backgroundImage: 'https://source.unsplash.com/78A265wPiO4',
+    featureImages: [RewardYourselfSlide1, RewardYourselfSlide2, RewardYourselfSlide3, RewardYourselfSlide4, RewardYourselfSlide5, RewardYourselfSlide6, RewardYourselfSlide7],
+    buttons: [{
+        link: '/experience#reward-yourself',
+        text: 'Learn About Reward Yourself'
+    }],
+}, {
+    title: 'Replay Analyzer',
+    intro: 'Replay Analyzer is a desktop application which allows users to upload & save their Rainbow Six Siege replays, and then analyze them to see how they performed. This application was created as a way to learn about desktop development, and was created for an eSports team which I coach.',
+    primaryTiles: [],
+    backgroundImage: 'https://source.unsplash.com/mcSDtbWXUZU',
+    featureImages: [ReplayAnalyzerSlide1, ReplayAnalyzerSlide2, ReplayAnalyzerSlide3, ReplayAnalyzerSlide4],
+    buttons: [{
+        link: 'https://github.com/Ross-Alexandra/replay-analyzer/',
+        text: 'View on GitHub'
+    }, {
+        link: '/experience#replay-analyzer',
+        text: 'Learn About Replay Analyzer'
+    }],
+}];
---
+++ src/pages/home/components/portfolio-page.tsx
@@ -0,0 +1,285 @@
+import styled from '@emotion/styled';
+import React from 'react';
+
+import { defaultFont, layerColor, maxPhoneBreakpoint, maxSmallPhoneBreakpoint, portfolioBackground } from "../../../theme";
+import { primaryButtonCSS } from "../../../elements";
+import { ExternalLink } from "../../../assets";
+import { PortfolioPageInfo } from '../../../app-data/portfolio-items';
+import { Carousel } from '../../../ui-atoms/carousel';
+
+const Wrapper = styled.div<{backgroundImage: string}>`
+    display: flex;
+    flex-direction: column;
+
+    .portfolio-page {
+        display: grid;
+        place-items: center;
+        position: relative;
+
+        min-height: 100svh;
+
+        padding: 15px 30px;
+
+        overflow: hidden;
+
+        ::before {
+            content: "";
+            
+            position: absolute;
+            inset: 0px;
+
+            background-image: url("${({backgroundImage}) => backgroundImage}");
+            background-size: cover;
+            background-position: center;
+            background-repeat: no-repeat;
+            background-attachment: fixed;
+
+            opacity: 0.35;
+            filter: blur(10px);
+            z-index: -1;
+        }
+
+        @media (max-width: ${maxPhoneBreakpoint}px) {
+            padding: 5px;
+        }
+
+        h2 {
+            font-size: 35px;
+        }
+
+        & > div {
+            position: relative;
+
+            display: flex;
+            flex-direction: column;
+            gap: 10px;
+            
+            width: fit-content;
+            min-width: 50%;
+            padding: 15px;
+            border-radius: 10px;
+
+            background-image:
+                linear-gradient(90deg, ${layerColor}, ${layerColor}),
+                linear-gradient(90deg, ${portfolioBackground}, ${portfolioBackground});
+
+            @media (max-width: ${maxPhoneBreakpoint}px) {
+                position: relative;
+                top: unset;
+                left: unset;
+                transform: unset;
+
+                max-height: unset;
+            }
+
+            h2, h3, p {
+                align-self: flex-start;
+
+                max-width: 900px;
+            }
+        }
+
+        .primary-tiles {
+            display: grid;
+            grid-template-columns: repeat(4, 1fr);
+
+            gap: 30px;
+
+            @media (max-width: ${maxPhoneBreakpoint}px) {
+                grid-template-columns: repeat(2, 1fr);
+            }
+
+            @media (max-width: ${maxSmallPhoneBreakpoint}px) {
+                grid-template-columns: 1fr;
+            }
+
+            img {
+                width: 150px;
+                aspect-ratio: 1;
+                border-radius: 10px;
+            }
+
+            .primary-tile {
+                display: flex;
+                flex-direction: column;
+                align-items: center;
+
+                gap: 5px;
+            }
+
+            .tile-info {
+                display: flex;
+                flex-direction: row;
+                width: 100%;
+
+                align-items: center;
+                justify-content: space-between;
+            }
+        }
+
+        .buttons {
+            align-self: end;
+
+            display: grid;
+            grid-auto-flow: column;
+            grid-auto-columns: 1fr;
+            align-items: center;
+            gap: 10px;
+
+            width: 100%;
+
+            @media (max-width: ${maxPhoneBreakpoint}px) {
+                grid-auto-flow: row;
+                grid-auto-rows: 1fr;
+
+                place-items: center;
+            }
+
+            a {
+                width: 100%;
+            }
+        }
+
+        a:not(.button) {
+            text-decoration: none;
+            cursor: pointer;
+
+            width: 100%;
+            font-family: ${defaultFont};
+            color: white;
+
+            display: flex;
+            flex-direction: row;
+            align-items: center;
+            justify-content: center;
+            gap: 5px;
+        }
+
+        a.button {
+            ${primaryButtonCSS}
+            text-decoration: none;
+        }
+
+        .featured-tile {
+            display: flex;
+            flex-direction: column;
+            margin-top: 10px;
+            gap: 10px;
+            
+            align-items: center;
+        }
+
+        .featured-image {
+            img {
+                max-height: 300px;
+                border-radius: 10px;
+
+                cursor: zoom-in;
+            }
+
+            .hover {
+                display: none;
+                position: absolute;
+                top: 50%;
+                left: 50%;
+                transform: translate(-50%, -50%);
+
+                height: 95%;
+
+                max-height: unset;
+                max-width: 95%;
+
+                z-index: 1;
+
+                cursor: zoom-out;
+                @media (max-width: ${maxPhoneBreakpoint}px) {
+                    height: unset;
+                    width: 95%;
+                }
+
+                &.focused {
+                    display: unset;
+                }
+            }
+        }
+    }
+`;
+
+type PortfolioPageProps = Omit<React.HTMLProps<HTMLDivElement>, 'as'> & PortfolioPageInfo;
+
+export const PortfolioPage: React.FC<PortfolioPageProps> = ({
+    title,
+    intro,
+    primaryTiles,
+    featureTitle,
+    featureDescription,
+    featureImages,
+    buttons,
+    ...props
+}) => {
+    const [focusedImage, setFocusedImage] = React.useState<string | undefined>(undefined);
+
+    return (
+        <Wrapper {...props}>
+            <div id={title.toLowerCase().replaceAll(' ', '-')} className='portfolio-page'>
+                <div>
+                    <h2>{title}</h2>
+                    <p>{intro}</p>
+
+                    <div className="primary-tiles">
+                        {primaryTiles.map((tile, index) => 
+                            <div key={index} className="primary-tile">
+                                <img src={tile.image} alt={tile.title} />
+                                <div className='tile-info'>
+                                    {tile.externalLink && 
+                                        <a
+                                            href={tile.externalLink}
+                                            target="_blank"
+                                            rel="noopener noreferrer"
+                                        >
+                                            {tile.title}
+                                            <ExternalLink width={15} height={15} />
+                                        </a>
+                                    }
+                                </div>
+                            </div>
+                        )}
+                    </div>
+
+                    <div className='featured-tile'>
+                        {featureTitle && <h3>{featureTitle}</h3>}
+                        {featureDescription && <p>{featureDescription}</p>}
+                        <div className='featured-image'>
+
+                            <Carousel 
+                                images={featureImages}
+                                onClickActiveImage={(image) => setFocusedImage(image)}
+                            />
+                            
+                            <img 
+                                className={`hover ${focusedImage ? 'focused' : ''}`}
+                                src={focusedImage}
+                                alt="Pick Ban Hover Effect"
+                                onClick={() => setFocusedImage(undefined)}
+                            />
+                        </div>
+                    </div>
+
+                    <div className='buttons'>
+                        {buttons.map((button, index) =>
+                            <a
+                                key={index}
+                                className='button'
+                                href={button.link}  
+                                target="_blank"
+                                rel="noopener noreferrer"
+                            >
+                                {button.text}
+                            </a>
+                        )}
+                    </div>
+                </div>
+            </div>
+        </Wrapper>
+    );
+};
